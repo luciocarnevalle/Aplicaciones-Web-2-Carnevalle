4dfa66c4bbad187305d08c248c48d01d6c187ece
Lucio Carnevalle <luciocarnevalle12@gmail.com>
2026-04-18 13:41:36 -0300

    Segundo commit

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
+Nombre: Lucio
+Apellido: Carnevalle 
+
 # Aplicaciones-Web-2-Carnevalle
+
+http://localhost:5000/
+
+GET1: Ver todos los productos
+http://localhost:5000/productos
+
+
+GET2: Ver un producto especifico
+http://localhost:5000/productos/wils-03 o http://localhost:5000/productos/babo-02
+
+
+POST1: Crear un usuario
+http://localhost:5000/usuarios
+
+//Datos para crear un usuario
+{
+  "nombre": "Rafael",
+  "apellido": "Nadal",
+  "email": "rafa.nadal@tenis.com",
+  "contraseña": "VamosRafa2024",
+  "edad": 37
+}
+
+
+POST2: Comprobar que se creo el usuario
+http://localhost:5000/usuarios/login
+
+//Datos para comprobar que se creo y ver su informacion
+{
+  "email": "rafa.nadal@tenis.com",
+  "contraseña": "VamosRafa2024"
+}
+
+
+PUT: Cambiar el precio de algun producto
+http://localhost:5000/productos/lux-01
+
+//Precio nuevo para probar y cambiarlo (ahora sale 135000)
+{
+  "precio": 140000
+}
+
+
+DELETE: Borrar un usuario poniendo atencion a la integridad de datos
+
+http://localhost:5000/usuarios/usr-001 (No se va a poder borrar porque tiene ventas asociadas)
+
+http://localhost:5000/usuarios/usr-007 (Este si se va a borrar)
+
+
--- index.js
+++ index.js
@@ -1,2 +1,238 @@
-console.log("Hola Mundo");
-console .log("Que onda");
+/* 
+
+//codigo con GET
+app.get(`/ColorDe/:objeto`, (req,res)=>{ //objeto es parametro en este caso. 
+    //ColorDe es la ruta
+    const obj = req.params.objeto //req.params es un objeto que 
+    // contiene los parametros que se le pasan a la ruta. En este caso, el parametro 
+    // es objeto, y lo guardo en la variable obj.
+
+    const resultado = cosas.find(e => e.name === obj) //busco en el array cosas el objeto
+    //  que tenga el mismo nombre que el parametro que me pasaron.
+
+    if (resultado)
+    {
+        res.status(200).json(resultado) //si encuentro el objeto, le envio una respuesta con status 200 y el objeto en formato JSON.
+    }else{
+        res.status(400).json({message: ` ${obj} Objeto no encontrado`})//si no encuentro el objeto, le envio una respuesta con status 400 (Bad Request)
+    }
+
+
+    //Codigo con POST
+    app.post(`/ColorDePost`, (req, res) => {
+     
+        const obj = req.body.objeto
+        const resultado = cosas.find(e => e.name === obj) //busco en el array cosas el objeto que tenga el mismo nombre que el parametro que me pasaron.
+
+    if (resultado)
+    {
+        res.status(200).json(resultado) //si encuentro el objeto, le envio una respuesta con status 200 y el objeto en formato JSON.
+    }else{
+        res.status(400).json({message: ` ${obj} Objeto no encontrado`})//si no encuentro el objeto, le envio una respuesta con status 400 (Bad Request)
+    }
+
+    })
+}) 
+
+*/
+
+import express from 'express';
+import dotenv from 'dotenv';
+import fs, { readFile, writeFile } from 'fs/promises';
+import e from 'express';
+
+//configuro dotenv
+dotenv.config();
+
+//creo instancia de app
+const app = express();
+//configuro el puerto
+const port = process.env.PORT || 3000; 
+
+//Middleware para que el servidor entienda JSON
+app.use(express.json()); 
+
+// Levanto el servidor
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+});
+
+//Pruebo para  ver si el servidor funciona
+app.get('/', (req, res) => {
+    res.send('Servidor de E-commerce funcionando correctamente');
+});
+
+// Ruta para obtener todos los productos
+const file = await readFile('./data/productos.json', 'utf-8');
+const productosData = JSON.parse(file);
+
+//GET
+app.get(`/productos`, async (req,res) => {
+    try{
+        res.status(200).json(productosData)
+    }catch{
+        res.status(500).json({message: 'Error al obtener los productos'})
+    }
+})
+
+
+//GET por ID
+app.get(`/productos/:id`, async (req,res) =>{
+    const productoId = req.params.id
+    const productoEncontrado = productosData.find(p => p.id === productoId)
+
+    if (productoEncontrado)
+    {
+        res.status(200).json(productoEncontrado) 
+    }else{
+        res.status(404).json({message: `Producto no encontrado`})
+    }
+})
+
+
+
+
+//POST
+app.post(`/usuarios`, async (req,res) => {
+    try{
+        //Extraigo los datos del cuerpo de la solicitud
+        const { nombre, apellido, email, contraseña, edad } = req.body
+
+        //Leo el archivo de usuarios para obtener los datos actuales
+        const data = await readFile('./data/usuarios.json', 'utf-8');
+        const usuariosData = JSON.parse(data);
+
+        //Creo un nuevo usuario con un ID único
+        const nuevoUsuario = { 
+            id: `usr-00${usuariosData.length + 1}`, 
+            nombre, 
+            apellido, 
+            email, 
+            contraseña, 
+            edad,
+            cuenta_validada: false 
+        };
+
+        //Agrego el nuevo usuario al array de usuarios y escribo el archivo actualizado
+        usuariosData.push(nuevoUsuario);
+        await writeFile('./data/usuarios.json', JSON.stringify(usuariosData, null, 2));
+
+        res.status(201).json({
+            message: 'Usuario creado correctamente',
+            usuario: nuevoUsuario
+        })
+    } catch (error) {
+        console.error(error); 
+        res.status(500).json({ message: 'Error al crear el usuario' });
+    }
+})
+
+
+//POST2
+app.post(`/usuarios/login`, async (req,res) => {
+    try{    
+        //Extraigo los datos del cuerpo de la solicitud
+        const { email, contraseña } = req.body
+
+        //Leo el archivo de usuarios para obtener los datos actuales
+        const data = await readFile('./data/usuarios.json', 'utf-8');
+        const usuariosData = JSON.parse(data);
+
+        //Busco un usuario que coincida con el email y la contraseña proporcionados
+        const usuarioEncontrado = usuariosData.find(e => e.email === email && e.contraseña === contraseña)
+
+        if (usuarioEncontrado)
+        {
+            res.status(200).json({
+                message: 'Inicio de sesión exitoso',
+                usuario: {
+                    nombre: usuarioEncontrado.nombre,
+                    apellido: usuarioEncontrado.apellido,
+                    email: usuarioEncontrado.email
+                }
+            }) 
+        }else{
+            res.status(401).json({message: `email o contraseña inválida`})
+        }
+    }    catch (error) {
+        console.error(error); 
+        res.status(500).json({ message: 'Error al iniciar sesión' });
+    }
+})  
+
+
+
+//PUT
+app.put(`/productos/:id`,async (req,res) => {
+    //Extraigo el ID del producto de los parámetros de la ruta 
+    // y el nuevo precio del cuerpo de la solicitud
+    const productoId = req.params.id
+    const NuevoPrecio = req.body.precio
+
+    try {
+        const data = await readFile('./data/productos.json', 'utf-8');
+        const productosData = JSON.parse(data);
+
+        //Busco el índice del producto en el array de productos 
+        const index = productosData.findIndex(p => p.id === productoId)
+
+        if(index !== -1){
+            //Si encuentro el producto, actualizo su precio en el array productosData
+            productosData[index].precio = NuevoPrecio
+            
+            //el stringify sirve para convertir el objeto productosData a formato JSON
+           await writeFile('./data/productos.json', JSON.stringify(productosData, null, 2));
+            res.status(200).json({message: `Precio actualizado correctamente`})
+        }else{
+            res.status(400).json({message: `No se encontro el ID del producto`})
+        }
+        } catch (error) {
+        res.status(500).json({message: `Error al actualizar el precio`, error})
+    }
+})
+
+
+//DELETE
+app.delete(`/usuarios/:id`, async (req,res) => {
+    const usuarioId = req.params.id
+
+    try{
+        //leo los dos jsons
+        const dataUsuarios = await readFile('./data/usuarios.json', 'utf-8');
+        const dataVentas = await readFile('./data/ventas.json', 'utf-8');
+
+        //parseo los datos de los jsons a objetos de JavaScript
+        const usuarios = JSON.parse(dataUsuarios);
+        const ventas = JSON.parse(dataVentas);
+
+        //verifico si el usuario tiene ventas asociadas 
+        const tieneVentas = ventas.some(v => v.id_usuario === usuarioId)
+
+        if(tieneVentas)
+        {
+                //Si tiene ventas no lo dejo borrar
+                res.status(403).json({ 
+                message: "No se puede eliminar el usuario porque tiene ventas asociadas. Primero debe gestionar el historial de ventas." 
+            });
+        }
+
+        //Uso filter en vez de splice porque filter me devuelve un nuevo array 
+        // sin el usuario que quiero eliminar, mientras que splice modifica 
+        // el array original y puede ser mas complicado de manejar en este caso.
+        const usuarioEncontrado = usuarios.filter(u => u.id !== usuarioId)
+
+        //Si el usuario encontrado es menor al array original, 
+        // significa que se elimino correctamente
+        if(usuarioEncontrado.length < usuarios.length){
+            await writeFile('./data/usuarios.json', JSON.stringify(usuarioEncontrado, null, 2));
+            res.status(200).json({message: `Usuario eliminado correctamente`})
+        }
+        else{
+            res.status(404).json({message: `Usuario no encontrado`})
+        }
+    }catch(error){
+        res.status(500).json({message: `Error al eliminar el usuario`})
+    }
+})
+
+
